b21ddb22e9c6932dbe76130d48e0b7c24e71d105
Hyeongjong Kim <jonkim0309@gmail.com>
2025-09-06 14:40:25 +0900

    chore(admin): add theme-provider

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@/components/admin/theme-provider";
+
 export const metadata = {
   robots: { index: false, follow: false, nocache: true },
 };
@@ -8,8 +10,17 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
