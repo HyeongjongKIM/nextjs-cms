05f17e1eaaa2c31de00e9ed77d4ecac2112f4f9b
Hyeongjong Kim <jonkim0309@gmail.com>
2025-09-09 00:01:38 +0900

    chore: add iron session

---
+++ lib/session.ts
@@ -0,0 +1,9 @@
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+
+export async function getSession() {
+  return getIronSession<{ id: string }>(await cookies(), {
+    cookieName: "nextjs-cms",
+    password: process.env.COOKIE_PASSWORD!,
+  });
+}
