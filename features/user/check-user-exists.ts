436839108e53cd4d9fc585cae4a87ed44aea0460
Hyeongjong Kim <jonkim0309@gmail.com>
2025-09-09 00:38:20 +0900

    feat(admin): implement route guarding with server-side redirects based on auth/session state

---
+++ features/user/check-user-exists.ts
@@ -0,0 +1,11 @@
+import { prisma } from "@/lib/prisma";
+
+async function checkUserExists() {
+  const user = await prisma.user.findFirst();
+  if (user) {
+    return true;
+  }
+  return false;
+}
+
+export { checkUserExists };
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "./lib/session";
 
-export function middleware(request: NextRequest) {
-  console.log("from middleware:::", request.url);
+const publicOnlyUrls = ["/admin/signup", "/admin/signin"];
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const isUserLoggedIn = "id" in session;
+  const isAdminRoot = request.nextUrl.pathname === "/admin";
+  const isPublicOnlyUrl = publicOnlyUrls.includes(request.nextUrl.pathname);
+
+  if (isUserLoggedIn) {
+    if (isPublicOnlyUrl || isAdminRoot) {
+      return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+    }
+  } else {
+    if (!isPublicOnlyUrl || isAdminRoot) {
+      return NextResponse.redirect(new URL("/admin/signin", request.url));
+    }
+  }
 
   return NextResponse.next();
 }
