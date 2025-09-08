fdbe1b1de686418d4c41ab6a4a414dc8d4e9655e
Hyeongjong Kim <jonkim0309@gmail.com>
2025-09-09 03:06:13 +0900

    feat(admin): add sidbar to admin page layout

--- app/admin/(app)/layout.tsx
+++ app/admin/(app)/layout.tsx
@@ -1,3 +1,91 @@
+'use client'
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
+import { Home, User, Image as MediaIcon, Files } from 'lucide-react'
+import Link from 'next/link'
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="bg-red-600">{children}</div>
+  return (
+    <SidebarProvider>
+      <Sidebar collapsible="icon">
+        <SidebarHeader className="flex-row justify-start group-data-[collapsible=icon]:justify-center items-center gap-2">
+          <SidebarTrigger />
+          <span className="group-data-[collapsible=icon]:hidden font-semibold truncate">
+            Admin Panel
+          </span>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/admin/dashboard">
+                    <Home />
+                    <span>Dashboard</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupLabel>Collections</SidebarGroupLabel>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/admin/collections/users">
+                    <User />
+                    <span>Users</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/admin/collections/media">
+                    <MediaIcon />
+                    <span>Media</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/admin/collections/pages">
+                    <Files />
+                    <span>Pages</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <User />
+                <span>Admin User</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+      <SidebarInset>
+        <main className="flex flex-col h-full overflow-auto p-2">
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
