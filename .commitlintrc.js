64aeb3c98aade9644045701ef0b65151f6b333eb
Hyeongjong Kim <jonkim0309@gmail.com>
2025-09-16 22:18:21 +0900

    chore: clean up commitlint rules and extend header length

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -5,17 +5,17 @@ module.exports = {
       2,
       'always',
       [
-        'feat', // 새로운 기능
-        'fix', // 버그 수정
-        'docs', // 문서 수정
-        'style', // 코드 스타일 변경
-        'refactor', // 리팩토링
-        'test', // 테스트 추가/수정
-        'chore', // 기타 작업
-        'perf', // 성능 개선
-        'ci', // CI 설정 변경
-        'build', // 빌드 관련 변경
-        'revert', // 이전 커밋 되돌리기
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'perf',
+        'ci',
+        'build',
+        'revert',
       ],
     ],
     'type-case': [2, 'always', 'lower-case'],
@@ -28,6 +28,6 @@ module.exports = {
     ],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 100],
   },
 };
